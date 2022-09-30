b77c2718fca910c3180157f929a0743459650555
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2022-09-30 23:29:59 +0200

    Tv show level 100
    Tv show level 100 done (displaying all the episode )

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+//Retrieve the JSON
+const rootContainer = document.querySelector("#root");
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+fetch("https://api.tvmaze.com/shows/82/episodes")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((episode) => {
+      //create episode container
+      const episodeContainer = document.createElement("div");
+      const episodeNameContainer = document.createElement("div");
+      const episodeName = document.createElement("h3");
+      const episodeContentContainer = document.createElement("div");
+      const episodeImg = document.createElement("img");
+      const episodeDescription = document.createElement("div");
 
-window.onload = setup;
+      //add class to each container
+      episodeContainer.className = "episode-container";
+      episodeNameContainer.className = "episode-name-container";
+      episodeName.className = "episode-name";
+      episodeContentContainer.className = "episode-content-container";
+      episodeImg.className = "episode-img";
+      episodeDescription.className = "episode-description";
+
+      console.log(episodeContentContainer);
+      //add content
+      episodeName.innerHTML = episode.name;
+      episodeImg.src = episode.image.medium;
+      episodeDescription.innerHTML = episode.summary;
+
+      //append element
+      episodeNameContainer.appendChild(episodeName);
+      episodeContentContainer.append(episodeImg, episodeDescription);
+      episodeContainer.append(episodeName, episodeContentContainer);
+      rootContainer.append(episodeContainer);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
